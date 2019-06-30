254fee8feb30fff706ec9bdc55eb6a3e2194bd2c
EuronRomanov <51369592+EuronRomanov@users.noreply.github.com>
2019-06-30 18:13:30 -0500

    Add files via upload

---
+++ module4-solution/src/menuRestaurant/categories.controller.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('MenuListController', MenuListController);
+
+MenuListController.$inject = ['items'];
+function MenuListController( items) {
+  var mainList = this;
+  mainList.items = items;
+}
+
+})();
---
+++ module4-solution/src/route.js
@@ -0,0 +1,48 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuRestaurant/templates/main.template.html'
+  })
+
+  // Premade list page
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuRestaurant/templates/menulist.template.html',
+    controller: 'MenuListController as mainList',
+    resolve: {
+      items: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  // Item detail
+  .state('items', {
+    url: '/items/{category}',
+    templateUrl: 'src/menuRestaurant/templates/items.template.html',
+    controller: 'ItemsController as itemDetail',
+    resolve: {
+      items: ['MenuDataService', '$stateParams', function (MenuDataService, $stateParams) {
+        return MenuDataService.getItemsForCategory($stateParams.category);
+      }]
+    }
+  });
+
+}
+
+})();
